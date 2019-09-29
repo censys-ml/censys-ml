f7f399e5a83f34d27e7d563c462376db4b6e4897
dagmawim <dmm474@drexel.edu>
2019-09-29 22:28:53 +0300

    Added initial model parsing changes

--- censys_ml/update_model.py
+++ censys_ml/update_model.py
@@ -3,11 +3,11 @@ import json
 from pprint import pprint
 from google.cloud import bigquery
 
-import config
+from censys_ml import utils
 
-global_config = config.get_config()
+global_config = utils.get_config()
 
-# dataset_ref = client.dataset(dataset_id, project=project)
+
 # def data_from_json(file):
 #     with open(file, 'r') as in_file:
 #         return json.load(in_file)
@@ -56,15 +56,6 @@ global_config = config.get_config()
 #     return list({f.split('.')[0] for f in fields})
 
 
-# def recurse_schema(field):
-#     if len(field.fields) == 0:
-#         return [field.name]
-#     else:
-#         return [
-#             field.name + '.' + r
-#             for f in field.fields
-#             for r in recurse_schema(f)
-#         ]
 
 
 # def get_dry_run(field, table):
@@ -90,8 +81,8 @@ global_config = config.get_config()
 
 #     for t, table in enumerate(tables):
 #         print "\n[X] Table {}".format(table)
-#         schema = client.get_table(dataset_ref.table(table)).schema
-#         columns = only_parents({item for t in schema for item in recurse_schema(t)})
+#         
+#         columns = only_parents()
 #         for i, field in enumerate(only_parents(fields)):
 #             print "[X] Field {}.{} ) {}".format(t, i, field)
 #             if field in costs:
@@ -104,9 +95,77 @@ global_config = config.get_config()
     # print("{} query will process {} bytes.".format(f, avg_proc))
 
 
+def parse_schema(fields):
+    result = {}
+    for field in fields:
+        j_field = field
+
+        if 'fields' in j_field and len(j_field['fields']) != 0:
+            parsed = parse_schema(j_field['fields'])
+            for k in parsed:
+                result[j_field['name'] + '.' + k] = parsed[k]
+        else:
+            result[j_field['name']] = j_field
+
+    return result
+
+
+# def type_precedence(item1, item2):
+#     # If the type changed over time, return the type with highest number
+#     precedence = {
+#         'STRING': 0,
+#         'INTEGER': 1
+#     }
+#     return item1
+
+
+def update_model_definition(json_schema, model_definition):
+    # TODO : handle model changes over time (str in 2018 -> int in 2019)
+    for k in json_schema:
+        model_definition[k] = json_schema[k]
+
+        # if k in model_definition:
+        #     model_definition[k] = type_precedence(item1=json_schema[k],
+        #                                           item2=model_definition[k])
+        # else:
+
+
+def get_model_definition(client, dataset_ref):
+    model_definition = {}
+    total = len([0 for _ in client.list_tables(dataset_ref)])
+
+    utils.printProgressBar(iteration=0, total=total,
+                           prefix='Table : ', suffix='Complete',
+                           length=50)
+    for i, table in enumerate(client.list_tables(dataset_ref)):
+        # print("[x] Working on table : {}".format(table.table_id))
+        utils.printProgressBar(iteration=i, total=total,
+                               prefix='Table : {}'.format(table.table_id),
+                               suffix='Complete', length=50)
+
+        table_ref = dataset_ref.table(table.table_id)
+        raw_schema = client.get_table(table_ref).schema
+        json_schema = parse_schema([r.to_api_repr() for r in raw_schema])
+        update_model_definition(json_schema, model_definition)
+    return model_definition
+
+
+def save_model_definition(model_definition):
+    outfile = global_config['censys']['model_outfile']
+    with open(outfile, 'w') as _file:
+        json.dump(model_definition, _file, indent=2)
+
+
 def main():
+    print('[x] Gathering censys data model')
     service_file = global_config['censys']['service_file']
+    project = global_config['censys']['project']
+    dataset_id = global_config['censys']['dataset_id']
+
     client = bigquery.Client.from_service_account_json(service_file)
+    dataset_ref = client.dataset(dataset_id, project=project)
+    model_def = get_model_definition(client, dataset_ref)
+    save_model_definition(model_definition=model_def)
 
 
 if __name__ == "__main__":
---
+++ censys_ml/utils.py
@@ -0,0 +1,39 @@
+import configparser
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix='',
+                      suffix='', decimals=1,
+                      length=100, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+def get_config():
+    config = configparser.ConfigParser()
+    config.read('config/config.ini')
+    return config
+
+
+def main():
+    print([g for g in get_config()])
+
+
+if __name__ == "__main__":
+    main()
