7e2cf296bf2c3f9c7a9d31a4e73b19e02b09706b
yaredgdb123 <46164446+yaredgdb123@users.noreply.github.com>
2020-06-08 20:34:10 +0300

    Add files via upload

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-
-export INPUT="stdin"
+export INPUT="file"
 export OUTPUT="file"
 
+VECTOR_DIR="../vector"
+echo "Using the following as vector directory: $VECTOR_DIR"
+export DATA_DIR=$VECTOR_DIR"/data"
+export IN_FILE=$VECTOR_DIR"/input/*.json"
+export OUT_FILE=$VECTOR_DIR"/output/vector-%Y-%m-%d.json"
+
 # debug
-vector --config vector/vector.toml --verbose --threads 4
+vector --config $VECTOR_DIR/vector.toml --verbose --threads 4
 
 # prod
-# vector --config src/vector/vector.toml --quiet --require-healthy --threads 4
+#vector --config src/vector/vector.toml --quiet --require-healthy --threads 4
