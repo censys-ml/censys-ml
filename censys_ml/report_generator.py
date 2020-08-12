be3d388540ee34ac140deb0dc6f85d07df12dbea
yaredgdb123 <yaredgirmadb123@gmail.com>
2020-08-12 20:22:56 +0300

    added functionality for OHE

--- censys_ml/report_generator.py
+++ censys_ml/report_generator.py
@@ -18,26 +18,64 @@ dicty = {
 
 #the code to use the report builder (feed query data to the function) isnt written yet
 
-
-def reportbuilder (par):
-    global_config = utils.get_config()
+global_config = utils.get_config()
+def reportBuilder (par):
     report_key = global_config['censys']['report_key']
     with open(report_key) as f:
         key = json.load(f)
     
     responce = censys.ipv4.CensysIPv4(api_id=key["UID"], api_secret=key["secret"])  
     report = responce.report(par["query"], field=par["field"], buckets=par["buckets"])
-    print(type(report))
-    
     report_dir = global_config['censys']['report_dir']
+
+    #current date and time
+    now = datetime.now()
+    time = str(now)
+    time = time[0:13] +" "+ time[14:16] + " " + time[17:19]
+    with open(report_dir+"result"+time+" .json", 'w') as fp:
+        json.dump(report, fp)
+    return report
+
+def machineEncode (report):
+    data = report["results"]
+    count = len(data)
+    keys = []
+    for i in range (0,count):
+        temp = data[i]
+        keys.append(temp["key"])
+    header = []
+    for i in range (0,count):
+        header.append("is"+keys[i])
+    header.append("doc_count")
+    new_key = {}
+    for i in range (0,count):
+        segment = []
+        temp = data[i]
+        for k in keys:
+            if k == temp["key"]:
+                segment.append (1)
+            else:
+                segment.append (0)
+
+        segment.pop(count-1)
+        new_key[temp["key"]] = segment
+
+    new_list = []
+    new_list.append(header)
+    for i in range (0,count):
+        segment = []
+        temp = data[i]
+        docs = int (temp["doc_count"])
+        segment = (new_key[temp["key"]])
+        segment.append(docs)
+        new_list.append(segment)
 
     # current date and time
     now = datetime.now()
     time = str(now)
     time = time[0:13] +" "+ time[14:16] + " " + time[17:19]
-    print("time = ", time)
-    
-    with open(report_dir+"result"+time+" .json", 'w') as fp:
-        json.dump(report, fp)
-   
-reportbuilder(dicty)
+    report_dir = global_config['censys']['report_dir']
+    with open(report_dir+"result"+time+" OHE.json", 'w') as fp:
+       json.dump(new_list, fp)
+
+machineEncode(reportBuilder(dicty))
