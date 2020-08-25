4f7f6fb0735de16a75843da38d5aa9d7e461ed90
Michael-Girma <60641399+Michael-Girma@users.noreply.github.com>
2020-08-25 04:25:33 +0300

    added some more encodings and scripts

--- censys_ml/field_transforms/numeric_field_transform.py
+++ censys_ml/field_transforms/numeric_field_transform.py
@@ -1,71 +1,125 @@
 from censys_ml.field_transforms import utils
 
-def encode_SHA_support(input_field, output_field):
-    #SHA-1	Less than 2048 bits	—	—
-    #SHA-2	2048 bits or longer
+
+def one_hot_encode(input_field, output_field, options, function_name, field_prefix=""):
+    """
+    Generates lua lines that One-Hot encodes a field from its possible options
+    :param input_field: censys Field
+    :param input_field: flattened version of censys Field
+    :param options: array of the possible values the input field can have
+    :param function_name: the lua function that will encode the field
+    :param field_prefix: an optional string to be concatenated at the end of original field
+    :return:
+    """
     lines = []
-    SHA_versions = [
-        "SHA1"
-    ]#SHA-2 is left for one-hot encoding
-    for version in SHA_versions:
+    for option in options:
+        key = option
+        if type(option) == str:
+            key = f'"{option}"'
         lines.append(
-            f'event["{output_field}_supports_{version}"] = encode_SHA_support(event["{input_field}"])["{version}"]'
+            f'event["{output_field}_{field_prefix}{option}"] = {function_name}(event["{input_field}"])[{key}]'
         )
     return lines
 
 
-def encode_RSA_key_length(input_field, output_field):
-    # 512 bits	Low-strength key
-    #1024 bits	Medium-strength key
-    #2048 bits	High-strength key
-    #4096 bits	Very high-strength key
+def generate_relating_lines(input_field, output_field, function_name, field_prefix=""):
+    """
+    Generates lua lines that checks other fields related to input_field for feature engineering
+    :param input_field: censys Field
+    :param input_field: flattened version of censys Field
+    :param function_name: the lua function that will encode the field
+    :param field_prefix: an optional string to be concatenated at the end of original field
+    :return:
+    """
     lines = []
-    standard_RSA_key_sizes = [
-        512 ,1024, 2048
-    ] #4096 is left out for one-hot encoding
-    for size in standard_RSA_key_sizes:
-        lines.append(
-            f'event["{output_field}_is_{size}"] = encode_RSA_length(event["{input_field}"])["s{size}"]'
-        )
+    lines.append(
+        f'event["{output_field}_{field_prefix}"] = {function_name}(event, "{input_field}")'
+    )
+    return lines
+
+def generate_check_lines(input_field, output_field, function_name, field_prefix=""):
+    """
+    Generates lua lines that return bool on conditions set in the static script
+    :param input_field: censys Field
+    :param input_field: flattened version of censys Field
+    :param function_name: the lua function that will encode the field
+    :param field_prefix: an optional string to be concatenated at the end of original field
+    :return:
+    """
+    lines = []
+    lines.append(
+        f'event["{output_field}_{field_prefix}"] = {function_name}(event["{input_field}"])'
+    )
     return lines
 
 
-def encode_certificate_version(input_field, output_field):
+def extract_feature_from_field(input_field, output_field):
     lines = []
-    available_versions = [1,2] #3 is left for one-hot encoding
-    for version in available_versions:
-        lines.append(
-            f'event["{output_field}_{version}"] = encode_certificate_version(event["{input_field}"])["v{version}"]'
-        )
-    return lines
-    
-def encode_status_code(input_field, output_field):
-    lines = []
-    response_status = [
+    if 'rsa_public_key.length' in input_field or 'rsa_params.length' in input_field or 'dh_params.prime.length' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            512, 768, 1024, 2048, 3072, 4096
+            # 8192 has been added to the options after i saw some records on censys that have it
+        ], "encode_key_length", field_prefix="is_"))
+        lines.extend(one_hot_encode(input_field, output_field, [
+            "SHA1"
+        ], "encode_SHA_support", field_prefix="supports_"))
+    elif 'dh_params.prime.length' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            512, 768, 1024, 2048, 3072, 4096
+            # 8192 has been considered but left out of the options after i saw some records on censys that have it
+        ], "encode_key_length", field_prefix="is_"))
+    elif 'parsed.version' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            1, 2
+        ], "encode_certificate_version"))
+    elif 'ecdsa_public_key.length' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            256, 384 # 521 has been left out
+        ], "encode_key_length", field_prefix='is_'))
+    elif 'status_code' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
             'success',
             'redirection',
             'client_error',
             'server_error'
-        ]#'informational_response(1xx)' is left for one-hot encoding
-    for status in response_status:
-        lines.append(
-            f'event["{output_field}_{status}"] = encode_status_code(event["{input_field}"])["{status}"]'
-        )
+        ], "encode_status_code", field_prefix="is_"))
+    elif 'rsa_public_key.exponent' in input_field or 'rsa_params.exponent' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            2, 3, 5, 17, 257  # common exponents, 65537 is left out
+        ], "encode_RSA_exponent", field_prefix="is_"))
+    elif 'validity.length' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            "days", "weeks", "months"
+        ], "encode_validity_length", field_prefix="in_"))
+    elif 'session_ticket.lifetime_hint' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            "minutes", "hours", "days" # the same one_hot_encode function was used although the functionality is different
+        ], "encode_validity_length", field_prefix="in_"))
+    elif 'smb_version.major' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            1, 2  # smb major version groups 3 has been left out
+        ], "encode_version_major", field_prefix="is_"))
+        output_field = output_field[:-6]
+        lines.extend(generate_relating_lines(input_field, output_field, 'version_number', 'version'))
+    elif 'amqp.banner.version.major' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            0  # 1 is left out
+        ], "encode_version_major", field_prefix="is_"))
+        output_field = output_field[:-6]
+        lines.extend(generate_relating_lines(input_field, output_field, 'version_number', 'version'))
+    elif 'rdp.banner.version.major' in input_field:
+        lines.extend(one_hot_encode(input_field, output_field, [
+            1, 2, 3, 4, 5, 6, 7, 8, 9  # rdp major versions, 10 is left out
+        ], "encode_version_major", field_prefix="is_"))
+        output_field = output_field[:-6]
+        lines.extend(generate_relating_lines(input_field, output_field, 'version_number', 'version'))
+    elif 'bacnet.device_id.vendor.id' in input_field:
+        lines.extend(generate_check_lines(input_field, output_field, 'is_ashrae', 'is_ashrae'))
+    elif 'basic_constraints.max_path_len' in input_field:
+        output_field = output_field[:-13]
+        lines.extend(generate_check_lines(input_field, output_field, 'has_no_sub_CA', 'has_no_subordinate_CA'))
     return lines
-    
 
-def extract_feature_from_field(input_field, output_field):
-    lines = []
-    if 'rsa_public_key.length' in input_field:
-        lines.extend(encode_RSA_key_length(input_field, output_field))
-        lines.extend(encode_SHA_support(input_field, output_field))
-    elif 'tls.certificate.parsed.version' in input_field:
-        lines.extend(encode_certificate_version(input_field, output_field))
-    elif 'status_code' in input_field:
-        lines.extend(encode_status_code(input_field, output_field))
-    return lines
-
-    
 
 def generate_numeric_lines(input_field, output_field):
     lines = []
@@ -83,13 +137,25 @@ def main():
     strings = [
         'p993.imaps.tls.tls.certificate.parsed.version',
         'p993.imaps.tls.tls.chain.parsed.subject_key_info.rsa_public_key.length',
-        'p80.http.open_proxy.connect.status_code'
+        'p80.http.open_proxy.connect.status_code',
+        'p25.smtp.starttls.tls.certificate.parsed.subject_key_info.rsa_public_key.exponent',
+        'p443.https.tls.chain.parsed.extensions.basic_constraints.max_path_len',
+        'p445.smb.banner.smb_version.major',
+        'p5672.amqp.banner.version.major',
+        'p3389.rdp.banner.version.major',
+        'p47808.bacnet.device_id.vendor.id',
+        'p443.https.tls.server_key_exchange.rsa_params.length',
+        'p443.https.tls.server_key_exchange.dh_params.prime.length',
+        'p110.pop3.ssl_2.certificate.parsed.subject_key_info.ecdsa_public_key.length',
+        'p110.pop3.starttls.tls.session_ticket.lifetime_hint',
+        'p1433.mssql.banner.tls.certificate.parsed.validity.length',
+        'p1433.mssql.banner.tls.server_key_exchange.rsa_params.exponent'
     ]
     for string in strings:
         output_field = string.replace('.', '__')
         lines.extend(generate_numeric_lines(string, output_field))
     print('\n'.join(lines))
 
+
 if __name__ == '__main__':
     main()
-    
--- scripts/static/numeric_utils.lua
+++ scripts/static/numeric_utils.lua
@@ -1,22 +1,62 @@
-function encode_RSA_length(str)
+function encode_key_length(str)
     if str ~= nil and str ~= '' and str ~= ' ' then
-    	result = {
-            s512    = 0,
-            s1024   = 0,
-            s2048   = 0,
-            s4096   = 0
+    	str = tonumber(str)
+	print(str)
+	result = {
+            [256]   = 0,
+            [384]   = 0,
+            [521]   = 0,
+            [512]    = 0,
+	    [768]    = 0,
+            [1024]  = 0,
+            [2048]  = 0,
+            [3072]    = 0,
+            [4096]   = 0
         }
-	result['s'..str] = 1
+	result[str] = 1
 	return result
     else
 	return {
-            s512    = nil,
-            s1024   = nil,
-            s2048   = nil,
-            s4096   = nil
+            [256]   = nil,
+            [384]   = nil,
+            [521]   = nil,
+            [512]   = nil,
+	    [768]   = nil,
+            [1024]  = nil,
+            [2048]  = nil,
+            [3072]  = nil,
+            [4096]  = nil
         }
     end
 end
+
+
+function encode_RSA_exponent(str)
+    if str ~= nil and str ~= '' and str ~= ' ' then
+    	str = tonumber(str)
+	print(str)
+	result = {
+            [2]    = 0,
+	    [3]    = 0,
+            [5]  = 0,
+            [17]  = 0,
+            [257]    = 0,
+            [65537]   = 0
+        }
+	result[str] = 1
+	return result
+    else
+	return {
+            [2]   = nil,
+	    [3]   = nil,
+            [5]  = nil,
+            [17]  = nil,
+            [257]  = nil,
+            [65537]  = nil
+        }
+    end
+end
+
 
 
 function encode_SHA_support(str)
@@ -40,17 +80,50 @@ function encode_SHA_support(str)
     end
 end
 
+function encode_version_major(str)
+    if str ~= nil and str ~= '' and str ~= ' ' then
+    	str = tonumber(str)
+	result = {
+            [1]    = 0,
+	    [2]    = 0,
+	    [3]    = 0,
+	    [4]    = 0,
+            [5]  = 0,
+            [6]  = 0,
+            [7]  = 0,
+            [8]   = 0,
+            [9]   = 0,
+            [10]   = 0
+        }
+	result[str] = 1
+	return result
+    else
+	return {
+            [1]    = nil,
+	    [2]    = nil,
+	    [3]    = nil,
+	    [4]    = nil,
+            [5]  = nil,
+            [6]  = nil,
+            [7]  = nil,
+            [8]   = nil,
+            [9]   = nil,
+            [10]   = nil
+        }
+    end
+end
 
 
 
 function encode_certificate_version(str)
     if str ~= nil and str ~= '' and str ~= ' ' then
-    	result = {
-            v1   = 0,
-            v2   = 0,
-            v3   = 0
+    	str = tonumber(str)
+	result = {
+            [1]  = 0,
+            [2]  = 0,
+            [3]   = 0
         }
-	result['v'..str] = 1
+	result[str] = 1
 	return result
     else
 	return {
@@ -61,23 +134,60 @@ function encode_certificate_version(str)
     end
 end
 
-function is_ashrae(str)
+
+function encode_ecdsa_length(str)
     if str ~= nil and str ~= '' and str ~= ' ' then
-    	result = {
-            v555 = 1,
-            v666 = 1,
-            v777 = 1,
-            v888 = 1,
-            v911 = 1,
-            v999 = 1,
-            v1111 = 1,
+    	str = tonumber(str)
+	result = {
+            [256]   = 0,
+            [384]   = 0,
+            [521]   = 0
         }
-	return result['v'..str] and 1 or 0
+	result[str] = 1
+	return result
+    else
+	return {
+            [256]   = nil,
+            [384]   = nil,
+            [521]   = nil,
+        }
+    end
+end
+
+function encode_validity_length(str)
+    if str ~= nil and str ~= '' and str ~= ' ' then
+    	str = tonumber(str)
+	return {
+	    minutes = str // 60,
+	    hours = str // 3600,
+	    days = str // 86400,
+	    weeks = str // 604800,
+            months = str // 2.628e+6
+	} 
+    else
+	return {
+	    minutes = nil,
+	    hours = nil,
+    	    days = nil,
+	    weeks = nil,
+	    months = nil
+	}
+    end
+end
+
+function version_number(event, field)
+    if event[field] ~= nil and event[field] ~= '' and event[field] ~= ' ' then
+	major = event[field]
+	minor_field = string.gsub(field, "major", "minor")
+	minor = event[minor_field]
+	major = tonumber(major)
+	minor = tonumber(minor)
+	minor = minor / 10 
+	return major + minor
     else
 	return nil
     end
 end
-
 
 function encode_status_code(str)
     if str ~= nil and str ~= '' and str ~= ' ' then
@@ -112,7 +222,43 @@ function encode_status_code(str)
 end
 
 
---print (flatten_RSA_length('4096')["s512"])
+function is_ashrae(str)
+    if str ~= nil and str ~= '' and str ~= ' ' then
+    	str = tonumber(str)
+	result = {
+            [555] = 1,
+            [666]= 1,
+            [777]= 1,
+            [888]= 1,
+            [911]= 1,
+            [999]= 1,
+            [1111] = 1,
+        }
+	bool = result[str] and 1 or 0
+	return bool == 1
+    else
+	return nil
+    end
+end
+
+function has_no_sub_CA(str)
+    if str ~= nil and str ~= '' and str ~= ' ' then
+    	str = tonumber(str)
+	if str == 0 then
+	    return true
+        else
+	    return false
+	end
+    else
+	return nil
+    end
+end
+
+
+
+
+
+--print (flatten_RSA_length('4096')["512"])
 --print (flatten_RSA_length('4096')["v1024"])
 --print (flatten_RSA_length('4096')["v2048])
 --print (flatten_RSA_length('4096')["s4096"])
